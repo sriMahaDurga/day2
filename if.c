a0986e26158952fc845aca4e63ca1c1477006ad0
sriMahaDurga <srimahadurga2020@gmail.com>
2025-05-18 10:28:05 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("The number is positive.\n");
+    return 0;
+}
